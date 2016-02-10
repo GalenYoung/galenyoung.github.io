fe467f49e4df1ca55c72a4cd63d51fa93c77de0d
galen <ygg@live.cn>
2016-02-10 10:38:00 +0800

    网站大体结构已经完成,主页，编程页，软件页都已实现响应式，接下来要实现响应式的是个人思考页面

--- gulpfile.js
+++ gulpfile.js
@@ -17,13 +17,40 @@ gulp.task('LessToCss',function(){
     gulp.src('style/*.less') //该任务针对的文件
         .pipe(less()) //该任务调用的模块
         .pipe(autoprefixer('last 2 version','ie8')) //添加前缀
-        .pipe(gulp.dest('style/css')) //保存未压缩文件到我们指定的目录
+        .pipe(gulp.dest('style')) //保存未压缩文件到我们指定的目录
         .pipe(rename({suffix:'.min'})) //给文件添加.min后缀
         .pipe(minifycss())  //压缩样式文件
-        .pipe(gulp.dest('result')) //输出压缩文件到指定目录
+        .pipe(gulp.dest('style')) //输出压缩文件到指定目录
         .pipe(notify({message:'Styles task complete'})); //提醒任务完成
  });
+gulp.task('CssToMin',function(){
+    gulp.src('style/reset.v1.0.css') //该任务针对的文件
+        .pipe(rename({suffix:'.min'})) //给文件添加.min后缀
+        .pipe(minifycss())  //压缩样式文件
+        .pipe(gulp.dest('style')) //输出压缩文件到指定目录
+        .pipe(notify({message:'Styles task complete'})); //提醒任务完成
+ });
+gulp.task('softLessToCss',function(){
+    gulp.src('software/soft-style/*.less') //该任务针对的文件
+        .pipe(less()) //该任务调用的模块
+        .pipe(autoprefixer('last 2 version','ie8')) //添加前缀
+        .pipe(gulp.dest('software/soft-style')) //保存未压缩文件到我们指定的目录
+        .pipe(rename({suffix:'.min'})) //给文件添加.min后缀
+        .pipe(minifycss())  //压缩样式文件
+        .pipe(gulp.dest('software/soft-style')) //输出压缩文件到指定目录
+        .pipe(notify({message:'soft的less任务输出完成'})); //提醒任务完成
+ });
 
+gulp.task('pgmLessToCss',function(){
+    gulp.src('programme/pgm-style/*.less') //该任务针对的文件
+        .pipe(less()) //该任务调用的模块
+        .pipe(autoprefixer('last 2 version','ie8')) //添加前缀
+        .pipe(gulp.dest('programme/pgm-style')) //保存未压缩文件到我们指定的目录
+        .pipe(rename({suffix:'.min'})) //给文件添加.min后缀
+        .pipe(minifycss())  //压缩样式文件
+        .pipe(gulp.dest('programme/pgm-style')) //输出压缩文件到指定目录
+        .pipe(notify({message:'gpm的less任务输出完成'})); //提醒任务完成
+ });
 //js代码校验、合并和压缩
 gulp.task('scripts',function(){  //scripts任务
     return gulp.src('js/*.js')
@@ -46,12 +73,15 @@ gulp.task('images',function(){
 
 //定义默认任务
 gulp.task('default',function(){
-        gulp.start('LessToCss','scripts','images');
+        gulp.start('LessToCss','softLessToCss','pgmLessToCss','scripts','images','CssToMin');
 })
 
 //watch
 gulp.task('watch',function(){
+    gulp.watch('style/reset.v1.0.css',['CssToMin']); 
     gulp.watch('style/*.less',['LessToCss']);  //watch .less files
+    gulp.watch('software/soft-style/*.less',['softLessToCss']); 
+    gulp.watch('programme/pgm-style/*.less',['pgmLessToCss']); 
     gulp.watch('js/*.js',['scripts']);  //watch .js files
     gulp.watch('images/*',['images']);
     livereload.listen(); //Create LiveReload server
