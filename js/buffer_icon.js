96f99905e83a44fc0d9599d5da1bbd0e30525546
galen <ygalen@outlook.com>
2016-02-20 22:36:53 +0800

    解决缓冲动图bug

--- js/buffer_icon.js
+++ js/buffer_icon.js
@@ -1,9 +1,16 @@
-document.onreadystatechange = subSomething;
-function subSomething(){
-    if(document.readyState == "loading"){
-        var buffer_icon = document.createElement('span');
-		buffer_icon.className = "buffer-icon";
-		buffer_icon.setAttribute("id", "buffer-icon");
+(function(window,document,undefined){
+	var buffer_icon = document.createElement('span') ;
+		buffer_icon.className = "buffer-icon" ;
+	    buffer_icon.setAttribute("id", "buffer-icon");
 		document.body.appendChild(buffer_icon);
-    }
-}
+		
+	var buffer_mask = document.createElement('div');
+		buffer_mask.className = "buffer-mask";
+		buffer_mask.setAttribute("id", "buffer-mask");
+		document.body.appendChild(buffer_mask);
+	
+	window.onload = function(){
+		buffer_icon.style.display = "none";
+		buffer_mask.style.display = "none";	
+	}
+})(window,document)
