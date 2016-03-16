cc16f9afe14c262ba377711f8d34c7dc873590c7
galen <ygalen@outlook.com>
2016-03-17 01:13:35 +0800

    各主页添加卡通小特效

---
+++ js/sal-blink-effect.js
@@ -0,0 +1,27 @@
+;(function(window,document,undefined){
+	var $class = function(classname){ return document.getElementsByClassName(classname)};
+	var $id = function(id){ return document.getElementById(id)};
+
+	var sal_blink_wrap = $class("sal-blink-wrap");
+	var sal_blink_eyes = $class("sal-blink-eyes");
+	var sal_blink_close = $id("sal-blink-close");
+	var sal_blink = $id("sal-blink");
+
+	onmousemove = function(e){
+		var e = e || window.event ;
+		var eClientX = e.clientX;
+		var eClientY = e.clientY;  
+		for(var i = 0 ; i < sal_blink_wrap.length ;i++){
+			sal_blink_eyes[i].style.top  = (sal_blink_wrap[0].offsetHeight * e.clientY / window.innerHeight) + "px";
+			sal_blink_eyes[i].style.left  = (sal_blink_wrap[0].offsetWidth * e.clientX / window.innerWidth) + "px";
+		};
+	};
+	sal_blink.addEventListener("animationend",function(){
+		for(var j = 0; j < sal_blink_eyes.length ; j++){
+			sal_blink_eyes[j].style.display = "block";	
+		};
+	});
+	sal_blink_close.onclick = function(){
+		sal_blink.style.display = "none";
+	};
+})(window,document)
