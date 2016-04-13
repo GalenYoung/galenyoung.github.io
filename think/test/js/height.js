b5f7624cdd95fe91a35491c1c01c5f34a673130c
Galen <Galen>
2016-04-13 11:06:23 +0800

    d

--- think/test/js/height.js
+++ think/test/js/height.js
@@ -1,23 +1,25 @@
-;(function(window,document,undefined){
-	var $tag = function(tagname,parentSelector){
-		parentSelector === undefined ? parentSelector = document : parentSelector;
-		return parentSelector.getElementsByTagName(tagname);
-	};
-	var $class = function(classname,parentSelector){
-		parentSelector === undefined ? parentSelector = document : parentSelector;
-		return parentSelector.getElementsByClassName(classname);
-	};
-	var $id = function(id,parentSelector){
-		parentSelector === undefined ? parentSelector = document : parentSelector;
-		return parentSelector.getElementById(id);
-	};
+window.onload = function(){
+	;(function(window,document,undefined){
+		var $tag = function(tagname,parentSelector){
+			parentSelector === undefined ? parentSelector = document : parentSelector;
+			return parentSelector.getElementsByTagName(tagname);
+		};
+		var $class = function(classname,parentSelector){
+			parentSelector === undefined ? parentSelector = document : parentSelector;
+			return parentSelector.getElementsByClassName(classname);
+		};
+		var $id = function(id,parentSelector){
+			parentSelector === undefined ? parentSelector = document : parentSelector;
+			return parentSelector.getElementById(id);
+		};
 
-	//get Dom
-	wrap = $class("wrap");
-	//logic
-	for(let i = 0 ; i < wrap.length ; i++){
-		wrap[i].style.height = wrap[i].offsetWidth/3 + "px";
-		$tag("a",wrap[i])[0].style.lineHeight = wrap[i].offsetHeight + "px";
-	}
-	
-})(window,document);
+		//get Dom
+		wrap = $class("wrap");
+		//logic
+		for(let i = 0 ; i < wrap.length ; i++){
+			wrap[i].style.height = wrap[i].offsetWidth/3 + "px";
+			$tag("a",wrap[i])[0].style.lineHeight = wrap[i].offsetHeight + "px";
+		}
+		
+	})(window,document);
+};
