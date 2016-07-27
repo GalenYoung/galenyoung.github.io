5efe49b1b98e518a2d55626f634b13d342ee803a
“Galee <ygalen@outlook.com>
2016-07-27 17:49:24 +0800

    pgm&soft to require

---
+++ js/soft/soft-main.js
@@ -0,0 +1,14 @@
+require.config({
+	paths :{
+		'sal-blink-effect' : '../sal-blink-effect',
+		'solve_footer_Location' : '../solve_footer_Location',
+	},
+	
+});
+
+require(['sal-blink-effect'],function(SBE){
+	var sbe = new SBE.salBlinkEffect();
+});
+require(['solve_footer_Location'],function(SFL){
+	var sfl = new SFL.solveFooterLocation();
+});
