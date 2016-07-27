ac4d746f484e4af3b3756927945e0e04de2bfe82
“Galee <ygalen@outlook.com>
2016-07-27 15:18:06 +0800

    城市一片漆黑，谁都不能看见谁

--- js/sal-blink-effect.js
+++ js/sal-blink-effect.js
@@ -2,11 +2,24 @@ define(function(){
 	var salBlinkEffect = function(){
 		var $class = function(classname){ return document.getElementsByClassName(classname)};
 		var $id = function(id){ return document.getElementById(id)};
-
+		var SBE_html = [
+			'<div class="sal-blink" id="sal-blink">',
+		        '<span class="sal-blink-wrap sal-blink-wrap-top">',
+		            '<b class="sal-blink-eyes sal-blink-eye-left" id="sal-blink-eye-left"></b>',
+		        '</span>',
+		        '<span class="sal-blink-wrap sal-blink-wrap-bottom">',
+		            '<b class="sal-blink-eyes sal-blink-eye-right" id="sal-blink-eye-right"></b>',
+		        '</span>',
+		        '<span class="sal-blink-close" id="sal-blink-close"></span>',
+		    '</div>'
+		].join('');
+		document.body.appendChild(SBE_html);
+		
 		var sal_blink_wrap = $class("sal-blink-wrap");
 		var sal_blink_eyes = $class("sal-blink-eyes");
 		var sal_blink_close = $id("sal-blink-close");
 		var sal_blink = $id("sal-blink");
+
 
 		if(document.documentElement.clientWidth >= 1000){
 			sal_blink.style.display = "block";
