0e82db8f44c49917d802dba7879c25dd7f2c612b
galen <ygalen@outlook.com>
2018-10-04 15:22:01 +0800

    add markdown resolve

--- software/softMdToHtml.js
+++ software/softMdToHtml.js
@@ -8,22 +8,37 @@ marked.setOptions({
   }
 })
 
+function fixedText(title='Software Document', sign=1) {
+  return `---
+    layout: soft-into
+    title: ${title}
+    id: soft-${sign}
+    url: software/${sign}
+    css: <link rel="stylesheet" type="text/css" href="../software/soft-page${sign}/soft-page${sign}.css">
+    permalink: /software/${sign}
+    ---`;
+}
+
+
+
 fs.readdir(__dirname, function (err, files) {
   if (err) throw err;
 
   files.map(function (filename) {
-    if(fs.statSync(path.join(__dirname, filename)).isDirectory() && filename.includes('soft-page')) {
-      fs.readdir(path.join(__dirname, filename),function(error,inFiles){
+    if (fs.statSync(path.join(__dirname, filename)).isDirectory() && filename.includes('soft-page')) {
+      fs.readdir(path.join(__dirname, filename), function (error, inFiles) {
         if (error) throw error;
 
-        inFiles.map(function(file){
-          if(path.extname(file) === ".MD"){
-            fs.readFile(path.join(__dirname, filename,file),"utf8",function (mdError,data){
-              if(mdError) throw mdError;
- 
-              fs.writeFile(path.join(__dirname, filename,`${path.parse(file).name}.html`),marked(data),function(errorIn){
-                if (errorIn) throw errorIn;
-              })             
+        inFiles.map(function (file) {
+          if (path.extname(file) === ".MD") {
+            fs.readFile(path.join(__dirname, filename, file), "utf8", function (mdError, data) {
+              if (mdError) throw mdError;
+
+              fs.writeFile(path.join(__dirname, filename.trim().slice(-2), `${path.parse(file).name}.html`),
+              fixedText(path.parse(file).name,) + marked(data),
+                function (errorIn) {
+                  if (errorIn) throw errorIn;
+                })
             })
           }
         })
