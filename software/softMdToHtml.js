6266eda83b87f895d2d11843960bd564d8482b41
galen <ygalen@outlook.com>
2018-10-04 13:51:24 +0800

    add markdown compile tools

---
+++ software/softMdToHtml.js
@@ -0,0 +1,38 @@
+var marked = require('marked');
+var path = require('path');
+var fs = require("fs");
+
+marked.setOptions({
+  highlight: function (code) {
+    return require('highlight.js').highlightAuto(code).value
+  }
+})
+
+fs.readdir(__dirname, function (err, files) {
+  if (err) throw err;
+
+  files.map(function (filename) {
+    if(fs.statSync(path.join(__dirname, filename)).isDirectory() && filename.includes('soft-page')) {
+      fs.readdir(path.join(__dirname, filename),function(error,inFiles){
+        if (error) throw error;
+
+        inFiles.map(function(file){
+          if(path.extname(file) === ".MD"){
+            fs.readFile(path.join(__dirname, filename,file),"utf8",function (mdError,data){
+              if(mdError) throw mdError;
+ 
+              fs.writeFile(path.join(__dirname, filename,`${path.parse(file).name}.html`),marked(data),function(errorIn){
+                if (errorIn) throw errorIn;
+              })             
+            })
+          }
+        })
+      })
+    };
+  });
+
+
+});
+
+var markdownString = '```js\n console.log("hello"); \n```';
+var HTMLString = marked(markdownString);
